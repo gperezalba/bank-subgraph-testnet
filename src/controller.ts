07b22a18ca8e68b3532e0027b0285b504a9dd9e8
gperezalba <gperezalba94@gmail.com>
2020-06-10 17:03:33 +0200

    solve problem when controller adds newtoken for pi

--- src/controller.ts
+++ src/controller.ts
@@ -6,6 +6,9 @@ import { Token as TokenContract } from "../generated/templates/Token/Token"
 import { Token as TokenTemplate } from "../generated/templates"
 
 import { addTokenHolder, updateTokenBalance } from "./token"
+import { zeroBD } from "./helpers"
+
+const PI_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export function handleTokenCreated(event: NewToken): void {
     addToken(event.params.newToken);
@@ -16,13 +19,25 @@ export function addToken(tokenAddress: Address): void {
 
     if (token == null) {
         token = new Token(tokenAddress.toHexString());
-        let contract = TokenContract.bind(tokenAddress);
-    
-        token.tokenSymbol = contract.symbol();
-        token.tokenName = contract.name();
-        token.tokenDecimals = contract.decimals();
-        token.totalSupply = contract.totalSupply().toBigDecimal();
-        token.holders = [];
+
+        if (tokenAddress.toHexString() != PI_ADDRESS) {
+            
+            let contract = TokenContract.bind(tokenAddress);
+        
+            token.tokenSymbol = contract.symbol();
+            token.tokenName = contract.name();
+            token.tokenDecimals = contract.decimals();
+            token.totalSupply = contract.totalSupply().toBigDecimal();
+            token.holders = [];
+        } else {
+            token.tokenSymbol = "PI";
+            token.tokenName = "PI";
+            token.tokenDecimals = "18";
+            token.totalSupply = zeroBD();
+            token.holders = [];
+        }
+
+        
 
         TokenTemplate.create(tokenAddress);
     }
