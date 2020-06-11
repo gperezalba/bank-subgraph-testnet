a6d2897001dd3a77ecd4fe1b630b3edb406119f3
gperezalba <gperezalba94@gmail.com>
2020-06-11 16:58:38 +0200

    getBalance helper for pibalance had a mistake

--- src/helpers.ts
+++ src/helpers.ts
@@ -51,6 +51,8 @@ export function isNullEthValue(value: string): boolean {
 }
 
 export function getBalance(address: Address): BigDecimal {
-  let contract = Balance.bind(Address.fromHexString("0xD474378e5FcF23f498c433C7352394aD51A4CfE4") as Address);
-  return contract.getBalance(address).toBigDecimal();
+  let contractAddress = "0xD474378e5FcF23f498c433C7352394aD51A4CfE4";
+  //let contract = Balance.bind(Address.fromHexString(contractAddress) as Address);
+  //return contract.getBalance(address).toBigDecimal();
+  return BigDecimal.fromString('0');
 }
