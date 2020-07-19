a3ac0e21cae6f777cf97a03a9ffce42e018ff58e
gperezalba <gperezalba94@gmail.com>
2020-07-19 12:44:31 +0200

    add packable stuff

--- src/tokenBalance.ts
+++ src/tokenBalance.ts
@@ -170,14 +170,14 @@ export function updatePackableBalance(walletAddress: string, tokenAddress: strin
         packableBalance.wallet = walletAddress;
         packableBalance.packabeId = tokenAddress.concat("-").concat(tokenId);
 
-        let packableWallet = PackableWallet.load(tokenAddress.concat("-").concat(tokenId));
+        /*let packableWallet = PackableWallet.load(tokenAddress.concat("-").concat(tokenId));
         let balances = packableWallet.balances;
 
         if (!balances.includes(id)) {
             balances.push(id);
             packableWallet.balances = balances;
             packableWallet.save();
-        }
+        }*/
     }
 
     let pnft = PNFTInterface.bind(Address.fromHexString(tokenAddress) as Address);
