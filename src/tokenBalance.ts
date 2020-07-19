7de601385e6d31c5c2a05f63001ce38eb8f099fb
gperezalba <gperezalba94@gmail.com>
2020-07-19 17:06:59 +0200

    add packable stuff

--- src/tokenBalance.ts
+++ src/tokenBalance.ts
@@ -161,15 +161,14 @@ export function updatePackableTokenBalance(walletAddress: string, tokenAddress: 
 
 export function updatePackableBalance(walletAddress: string, tokenAddress: string, tokenId: string): void {
     let id = walletAddress.concat("-").concat(tokenAddress).concat("-").concat(tokenId);
-    let packableId = tokenAddress.concat(tokenId);
-    let packableIdEntity = PackableId.load(packableId);
+    let packableId = tokenAddress.concat("-").concat(tokenId);
 
     let packableBalance = PackableBalance.load(id);
 
     if (packableBalance == null) {
         packableBalance = new PackableBalance(id);
         packableBalance.wallet = walletAddress;
-        packableBalance.packabeId = packableIdEntity.id;
+        packableBalance.packabeId = packableId;
 
         let packableWallet = PackableWallet.load(walletAddress.concat(tokenAddress));
 
